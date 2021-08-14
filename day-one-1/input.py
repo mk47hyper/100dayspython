58869f8595a6a4ab005b30a01d9e5b8d4b8ab4c6
mk47hyper <mkhyper17@gmail.com>
2021-08-15 00:46:20 +0100

    Removing day 1

--- day-one-1/input.py
+++
@@ -1,6 +0,0 @@
-# print("Hello, " + input("What is your name? "))
-
-# name = input("What is your name: ")
-# print(len(name))
-
-print(len(input("What is your name? ")))
--- day-one-1/naming_variables.py
+++
@@ -1,9 +0,0 @@
-name = "Jack"
-print(name)
-
-name = "Angela"
-print(name)
-
-username = input("What is your name? ")
-length = len(username)
-print(length)
