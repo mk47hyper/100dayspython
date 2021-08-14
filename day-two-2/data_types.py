062c1b3e3c8cde2d8732a06f01e13854603a5772
mk47hyper <mkhyper17@gmail.com>
2021-08-15 00:40:03 +0100

    Introduction to python data types.

---
+++ day-two-2/data_types.py
@@ -0,0 +1,19 @@
+# Data Types:
+
+# String:
+print("123" + "345")
+
+# Integer:
+print(123 + 345)
+
+print(123_456_789)
+
+# Float:
+print(3.14159)
+
+# Boolean:
+# True
+# False
+
+street_name = "Abbey Road"
+print(street_name[4] + street_name[7])
