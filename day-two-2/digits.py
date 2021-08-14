59b30d8ec15d61010a4d6bcf49ea0b4601c4c2f5
mk47hyper <mkhyper17@gmail.com>
2021-08-15 00:53:02 +0100

    Cleaned day two from branch.

--- day-two-2/digits.py
+++
@@ -1,7 +0,0 @@
-two_digit_number = input("Type a two digit number: \n")
-
-# first_digit = two_digit_number[0]
-# second_digit = two_digit_number[1]
-# print(int(first_digit) + int(second_digit))
-
-print(int(two_digit_number[0]) + int(two_digit_number[1]))
