8bde964abbbca6c5e2c4c1887006c58b647ec56b
mk47hyper <mkhyper17@gmail.com>
2022-07-24 12:12:40 +0100

    Refactored pizza order and love calc.

--- Day3/pizzaorder.py
+++ Day3/pizzaorder.py
@@ -1,45 +1,26 @@
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_peperoni = input("Do you want peperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+size = input("What size pizza do you want? S, M, or L\n")
+add_peperoni = input("Do you want peperoni? Y or N\n")
+extra_cheese = input("Do you want extra cheese? Y or N\n")
 
 bill = 0
 
-if size == "S":
+if size == "S".lower():
     bill += 15
-elif size == "M":
+elif size == "M".lower():
     bill += 20
-elif size == "L":
+elif size == "L".lower():
     bill += 25
 else:
     bill += 25
 
-if add_peperoni == "Y":
+if add_peperoni == "Y".lower():
     if size == "S":
         bill += 2
     else:
         bill += 3
 
-if extra_cheese == "Y":
+if extra_cheese == "Y".lower():
     bill += 1
 
 print(f"You final bill is : ${bill}")
-
-
-# if size == "S":
-#     bill += 15
-#     if add_peperoni == "Y":
-#         bill += 2
-#         print(f"Small pizza coming right up.")
-#         if size == "M":
-#             bill = 20
-#             if add_peperoni == "Y":
-#                 bill += 3
-#                 print(f"Medium pizza coming right up.")
-#                 if size == "L":
-#                     bill = 25
-#                     if add_peperoni == "Y":
-#                         bill += 3
-#                         print(f"Large pizza coming right up.")
-#                     else:
-#                         print("Maybe next time.")
--- Day3/tickets.py
+++ Day3/tickets.py
@@ -1,14 +1,26 @@
 print("Welcome to the roller coaster!")
-height = (int(input("What is your height in cm? ")))
+height = (int(input("What is your height in cm?\n")))
+bill = 0
 
 if height >= 120:
     print("You can ride the roller coaster!")
-    age = int(input("What is your age? "))
+    age = int(input("What is your age?\n"))
+
     if age < 12:
         print("Please pay $5.")
+        bill = 5
     elif age <= 18:
         print("Please pay $7.")
+        bill = 7
     else:
         print("Please pay $12.")
+        bill = 12
+
+    wants_photo = input("Do you want a photo taken? Y or N.\n")
+    if wants_photo == "Y".lower():
+        # Add 3$ to their bill
+        bill += 3
+    print(f"Your final bill is {bill}")
+
 else:
     print("Sorry, you have to grow taller before you can ride.")
