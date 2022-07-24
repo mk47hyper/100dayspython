8a6afa851d20d39dfe1cdf8df322f2a9d6e85ccc
mk47hyper <mkhyper17@gmail.com>
2022-07-24 13:02:36 +0100

    Random module intro.

---
+++ Day4/random_module.py
@@ -0,0 +1,9 @@
+# Mersenne Twister
+# Pseudorandom number generators
+import random
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+random_float = random.random()
+print(random_float * 1000)
